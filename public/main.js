91596eac81f4e21cf96e3dfc9e7da2c4b33934a8
KPblCEHblLLl <kpblcenblw@gmail.com>
2017-09-04 23:01:36 +0300

    добавил редактирование

--- public/main.js
+++ public/main.js
@@ -1,6 +1,10 @@
+let form = $();
+let log = $();
+
 $(function() {
-	let form = $("#editor");
-	let log = $("#log-list");
+	form = $("#editor");
+	log = $("#log-list");
+
 	form.submit(function() {
 		let id = $(this).find("[name='id']").val();
 		let title = $(this).find("[name='title']").val();
@@ -29,17 +33,34 @@ $(function() {
 		return false;
 	});
 	loadLog(log);
+
+	$("body").on("click", ".log-item .edit", editLogItem);
 });
 
 function loadLog(log) {
 	$.ajax("/api/log-item").done(function(resp) {
 		let items = resp.map(function(logItem) {
-			return "<div class='log-item'>" +
+			return "<div class='log-item' data-id='" + logItem._id + "'>" +
+					"<div class='edit'></div>" +
 				logItem["created"] + " " + logItem["title"] +
 				"<div>" + logItem["text"].replace(/\n/g, "<br/>") + "</div>" +
 				"</div>"
 		});
 		log.html(items.join(""));
 	});
+}
 
+function editLogItem() {
+	let id = $(this.parentElement).data("id");
+	initEdit(id);
+}
+
+function initEdit(id) {
+	$.get("/api/log-item/" + id).done(function(response) {
+		let logItem = response.logItem;
+		form.find("[name='id']").val(id);
+		form.find("[name='title']").val(logItem.title);
+		form.find("[name='text']").val(logItem.text);
+
+	})
 }
